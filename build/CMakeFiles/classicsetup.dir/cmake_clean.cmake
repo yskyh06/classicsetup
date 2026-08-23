f076184e28eaf01b23a3f3195f3b2747b9ad279e
YHK <yskyh06@gmail.com>
2026-08-23 17:40:29 +0900

    chore: initialize ClassicSetup project

---
+++ build/CMakeFiles/classicsetup.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classicsetup.dir/link.d"
+  "CMakeFiles/classicsetup.dir/src/main.c.o"
+  "CMakeFiles/classicsetup.dir/src/main.c.o.d"
+  "classicsetup"
+  "classicsetup.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/classicsetup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
